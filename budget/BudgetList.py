92c04f1f5b9f14f90b882fc56f6658e1e8b31358
Yan Xu <yan.xu@sas.com>
2023-04-27 11:49:27 +0000

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,6 @@
 from . import Expense
+import matplotlib.pyplot as plt
+
 
 class BudgetList(list):
     def __init__(self, budget):
@@ -52,7 +54,16 @@ def main():
 
     for entry in myBudgetList:
         print(entry)
-        
+
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs. total budget')
+
+    plt.show()
 
 
 if __name__ == "__main__":
