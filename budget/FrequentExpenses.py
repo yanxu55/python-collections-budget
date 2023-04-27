17487f6f74aad73689cda2f465956bf9e99d57da
Yan Xu <yan.xu@sas.com>
2023-04-27 00:57:59 +0000

    Completed module

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,9 @@
+from . import Expense
+
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+
+spending_categories = []
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+    
