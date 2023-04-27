a998ba15a7a9d58c5f6673ef7d000597dd5d1535
Yan Xu <yan.xu@sas.com>
2023-04-27 01:50:44 +0000

    Completed module

---
+++ budget/BudgetList.py
@@ -0,0 +1,25 @@
+class BudgetList(list):
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+
+    # Path: corepy/python-collections-budget/budget/BudgetList.py
+    def append(self, item):
+        if self.sum_expenses + item < self.budget:
+            # Path: corepy/python-collections-budget/budget/BudgetList.py
+            super().append(item)
+            self.sum_expenses += item
+        else:
+            # Path: corepy/python-collections-budget/budget/BudgetList.py
+            self.overages.append(item)
+            self.sum_overages += item
+
+
+    def __len__(self):
+        return len(self.expenses) + len(self.overages)
+    
+    
