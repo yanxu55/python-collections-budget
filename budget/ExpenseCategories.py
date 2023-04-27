cc30d1b409c9d72d33598ba9a12c588101892091
Yan Xu <yan.xu@sas.com>
2023-04-27 13:06:16 +0000

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,5 +1,7 @@
 from . import Expense
 import matplotlib.pyplot as plt
+import timeit
+
 
 def main():
     expenses = Expense.Expenses()
@@ -14,6 +16,25 @@ def main():
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print('Sets are NOT equal by subset test')
+
+    print(timeit.timeit(stmt='expenses.categorize_for_loop()',
+                    setup=
+                    '''
+                    from . import Expense
+                    expenses = Expense.Expenses()
+                    expenses.read_expenses('data/spending_data.csv')
+                    ''',
+                    number=100000,
+                    globals=globals()))
+    
+
+    
+    
+
+
+
+
+
 
 
 if __name__ == "__main__":
