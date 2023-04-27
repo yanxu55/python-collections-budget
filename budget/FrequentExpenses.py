3b0d53f30bfa65bc08040437d9df2b743e858216
Yan Xu <yan.xu@sas.com>
2023-04-27 01:13:27 +0000

    Completed module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,4 +1,5 @@
 from . import Expense
+import collections
 
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
@@ -6,4 +7,13 @@ expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
-    
+
+spending_counter = collections.Counter(spending_categories)
+print(spending_counter)
+
+top5 = spending_counter.most_common(5)
+print(top5)
+
+categories, count = zip(*top5)
+print(categories, count)
+
